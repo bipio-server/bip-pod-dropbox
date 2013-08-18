c653c23d88fd855e9590f66741cc02dd6ad2be42
michael pearson <gravatar@x0rz.com>
2013-08-17 20:18:10 -0400

    stubbed

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * The Bipio Dropbox Pod.  Dropbox Actions and Content Emitters
+ * 
+ * @author Michael Pearson <michael@cloudspark.com.au>
+ * Copyright (c) 2010-2013 CloudSpark pty ltd http://www.cloudspark.com.au
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Pod = require('bip-pod'),
+    Dropbox = new Pod({
+        name : 'dropbox',
+        description : 'Dropbox',
+        authType : 'oauth'
+    });
+
+Dropbox.add(require('./generate_link.js'));
+Dropbox.add(require('./save_file.js'));
+
+// -----------------------------------------------------------------------------
+module.exports = Dropbox;
